cc0f128c35006c7cf4a63e206d7f1509489af791
Raunaq Singh <>
2023-01-17 15:41:28 -0600

    removed the test table and added the code for the javascript to loop through the data to get values

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const output = document.getElementById("output");
 
 let html = "";
 for (let i = 0; i < data.length; i++) {
-    html += `<p>${data[i].name}: ${data[i].recclass}</p>`;
+    // html += `<p>${data[i].name}: ${data[i].recclass}</p>`;
+    html += `<div class="box">
+    <div class="row">
+        <div class="col"> ID : ${data[i].id}</div>
+        <div class="col">Name : ${data[i].name}</div>
+    </div>
+    <div class="row"> Nametype : ${data[i].nametype}</div>
+    <div class="row">Class : ${data[i].recclass}</div>
+    <div class="row">Mass : ${data[i].mass} Tons</div>
+    <div class="row"> Fall : ${data[i].fall}</div>
+    <div class="row">Year : ${data[i].year}</div>
+    <div class="row">
+        <div class="col">Latitude : ${data[i].reclat}</div>
+        <div class="col">Longitude : ${data[i].reclong}</div>
+    </div>
+</div>`
 }
 output.innerHTML = html;
